9af9858696ad6c3f9c0d92e87adcae8eeebce1dc
Petr Jindra <petr@gbol.cz>
2023-11-30 15:57:03 +0100

    project renamed to the constrained_loaders

--- src/constrained_loaders/exceptions.py
+++ src/constrained_loaders/exceptions.py
@@ -5,31 +5,31 @@
 from __future__ import annotations
 
 
-class DatmodifException(Exception):
+class ConstrainedLoadersException(Exception):
     """Base exception for all other exception defined in this library."""
 
     pass
 
 
-class ExtensionNotFound(DatmodifException, LookupError, KeyError):
+class ExtensionNotFound(ConstrainedLoadersException, LookupError, KeyError):
     """Raised when extension was not found."""
 
     pass
 
 
-class SortNotFound(DatmodifException, LookupError, KeyError):
+class SortNotFound(ConstrainedLoadersException, LookupError, KeyError):
     """Raised when query sort was not found."""
 
     pass
 
 
-class FilterNotFound(DatmodifException, LookupError, KeyError):
+class FilterNotFound(ConstrainedLoadersException, LookupError, KeyError):
     """Raised when filter was not found."""
 
     pass
 
 
-class FieldNotDefined(DatmodifException, LookupError, KeyError):
+class FieldNotDefined(ConstrainedLoadersException, LookupError, KeyError):
     """Raised when field was not found (e.g. no filter is defined for the field)."""
 
     pass
--- src/constrained_loaders/loader_builders.py
+++ src/constrained_loaders/loader_builders.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 from abc import ABC
 from typing import Generic, Iterable, Self
 
-from crudalchemy.abstraction import DataSourceBuilder, T
+from constrained_loaders.abstraction import DataSourceBuilder, T
 
 
 class DataSourceBuilderBase(DataSourceBuilder[T], ABC, Generic[T]):

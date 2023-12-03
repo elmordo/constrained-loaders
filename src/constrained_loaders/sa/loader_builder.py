d847242b605b81b01630944af18ecc2d4438f5e9
Petr Jindra <petr@gbol.cz>
2023-12-03 11:02:02 +0100

    usage of the SALoader in the SALoaderBuilder

--- src/constrained_loaders/sa/loader_builder.py
+++ src/constrained_loaders/sa/loader_builder.py
@@ -9,13 +9,14 @@ from typing import Generic
 from sqlalchemy import Select
 from sqlalchemy.orm import Session
 
-from constrained_loaders import LoaderBuilderBase, Loader, T, LoaderSpec
+from constrained_loaders import LoaderBuilderBase, T, LoaderSpec
+from .loader import SALoader
 
 
 class SALoaderBuilder(LoaderBuilderBase[T, Select], Generic[T]):
     def __init__(self, spec: LoaderSpec[Select], bare_query: Select, session: Session):
         super().__init__(spec, bare_query)
-        self.session: Session = session
+        self._session: Session = session
 
     def set_offset(self, offset: int) -> None:
         self._query = self._query.offset(offset)
@@ -23,5 +24,5 @@ class SALoaderBuilder(LoaderBuilderBase[T, Select], Generic[T]):
     def set_limit(self, limit: int) -> None:
         self._query = self._query.limit(limit)
 
-    def build(self) -> Loader[T]:
-        pass
+    def build(self) -> SALoader[T]:
+        return SALoader(self._query, self._session)

b038a9ab2ef65501afe17d88495ea7e8c5f17a62
Petr Jindra <petr@gbol.cz>
2023-12-03 10:45:53 +0100

    the loader builder - added session as init argument

--- src/constrained_loaders/sa/loader_builder.py
+++ src/constrained_loaders/sa/loader_builder.py
@@ -7,11 +7,16 @@ from __future__ import annotations
 from typing import Generic
 
 from sqlalchemy import Select
+from sqlalchemy.orm import Session
 
-from constrained_loaders import LoaderBuilderBase, Loader, T
+from constrained_loaders import LoaderBuilderBase, Loader, T, LoaderSpec
 
 
 class SALoaderBuilder(LoaderBuilderBase[T, Select], Generic[T]):
+    def __init__(self, spec: LoaderSpec[Select], bare_query: Select, session: Session):
+        super().__init__(spec, bare_query)
+        self.session: Session = session
+
     def set_offset(self, offset: int) -> None:
         self._query = self._query.offset(offset)
 

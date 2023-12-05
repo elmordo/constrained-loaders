c3e797c24a6207b0324e8955a54a6c0347324e61
Petr Jindra <petr@gbol.cz>
2023-12-05 19:55:21 +0100

    simple filter and simple order

--- src/constrained_loaders/sa/simple_builder.py
+++ src/constrained_loaders/sa/simple_builder.py
@@ -4,12 +4,19 @@
 
 from __future__ import annotations
 
-from typing import Generic
+from typing import Generic, Optional, Any, Callable
 
 from sqlalchemy import Select
 from sqlalchemy.orm import Session
 
-from constrained_loaders import LoaderBuilderBase, T, LoaderSpec
+from constrained_loaders import (
+    LoaderBuilderBase,
+    T,
+    LoaderSpec,
+    QuerySort,
+    SortDirection,
+    QueryFilter,
+)
 from .loader import SALoader
 
 
@@ -30,3 +37,30 @@ class SASimpleLoaderBuilder(LoaderBuilderBase[T, Select], Generic[T]):
 
     def build(self) -> SALoader[T]:
         return SALoader(self._query, self._session)
+
+
+class SASimpleQuerySort(QuerySort[Select]):
+    def __init__(self, expr):
+        super().__init__()
+        self._expr = expr
+
+    def apply_sorting(self, query: Select, direction: SortDirection) -> Select:
+        if direction is SortDirection.UNSPECIFIED:
+            order_by = self._expr
+        elif direction is SortDirection.ASC:
+            order_by = self._expr.asc()
+        elif direction is SortDirection.DESC:
+            order_by = self._expr.desc()
+        else:
+            raise ValueError(f"Unknown sort direction '{direction}'")
+        return query.order_by(order_by)
+
+
+class SASimpleQueryFilter(QueryFilter[Select]):
+    def __init__(self, expr, filter_factory: Callable[[Any, Any], Any]):
+        super().__init__()
+        self._expr = expr
+        self._filter_factory = filter_factory
+
+    def apply_filter(self, query: Select, reference_value: Optional[Any]) -> Select:
+        return query.where(self._filter_factory(self._expr, reference_value))

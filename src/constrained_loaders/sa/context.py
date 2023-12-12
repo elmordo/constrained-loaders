c4cd64e9b456405f09d025dd01b3c5625edd9fb5
Petr Jindra <petr@gbol.cz>
2023-12-12 21:44:27 +0100

    the SA builder context + SA join extension

--- src/constrained_loaders/sa/context.py
+++ src/constrained_loaders/sa/context.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Dict
+from typing import Dict, Protocol, List
 
 from sqlalchemy import Select
 
@@ -9,14 +9,38 @@ class SALoaderBuilderContext:
     def __init__(self, initial_select: Select):
         self.main_query = initial_select
         self.sub_queries: Dict[str, Select] = {}
-        self._filters = []
-        self._sorts = []
+        self._filters: List[SABuilderContextApplicator] = []
+        self._sorts: List[SABuilderContextApplicator] = []
+        self._extensions: List[SABuilderContextApplicator] = []
 
-    def add_filter(self, filter_):
+    def add_filter(self, filter_: SABuilderContextApplicator):
         self._filters.append(filter_)
 
-    def add_sort(self, sort_):
-        self._sorts.append(sort_)
+    def add_sort(self, sort: SABuilderContextApplicator):
+        self._sorts.append(sort)
+
+    def add_extension(self, extension: SABuilderContextApplicator):
+        self._extensions.append(extension)
 
     def build_select(self) -> Select:
-        raise NotImplementedError()
+        self._apply_extensions()
+        self._apply_filters()
+        self._apply_sorts()
+        return self.main_query
+
+    def _apply_extensions(self):
+        for e in self._extensions:
+            e(self)
+
+    def _apply_filters(self):
+        for f in self._filters:
+            f(self)
+
+    def _apply_sorts(self):
+        for s in self._sorts:
+            s(self)
+
+
+class SABuilderContextApplicator(Protocol):
+    def __call__(self, context: SALoaderBuilderContext):
+        pass
--- src/constrained_loaders/sa/extensions.py
+++ src/constrained_loaders/sa/extensions.py
@@ -11,17 +11,23 @@ from constrained_loaders.sa.context import SALoaderBuilderContext
 
 
 class SAJoinExtension(QueryExtension[SALoaderBuilderContext]):
-    def __init__(self, join_to, sub_query_name: Optional[str] = None):
+    def __init__(self, join_to, sub_query_name: Optional[str] = None, on_clause=None):
         super().__init__()
+        self._on_clause = on_clause
         self._join_to = join_to
         self._sub_query_name = sub_query_name
+
+    def __call__(self, context: SALoaderBuilderContext) -> None:
+        if self._sub_query_name is None:
+            context.main_query = context.main_query.join(self._join_to, self._on_clause)
+        else:
+            sq = self._sub_query_name
+            context.sub_queries[sq] = context.sub_queries[sq].join(
+                self._join_to, self._on_clause
+            )
 
     def apply_extension(
         self, context: SALoaderBuilderContext
     ) -> SALoaderBuilderContext:
-        if self._sub_query_name is None:
-            context.main_query = context.main_query.join(self._join_to)
-        else:
-            q = context.sub_queries[self._sub_query_name]
-            context.sub_queries[self._sub_query_name] = q.join(self._join_to)
+        context.add_extension(self)
         return context

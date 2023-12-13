a5b8016764c3e8dadca5a39a1f33dc7f5df6f7db
Petr Jindra <petr@gbol.cz>
2023-12-13 12:28:34 +0100

    basic filter implementation

--- src/constrained_loaders/sa/filters.py
+++ src/constrained_loaders/sa/filters.py
@@ -1,13 +1,32 @@
 from __future__ import annotations
 
-from typing import Optional, Any
+import functools
+from typing import Optional, Any, List, Callable
 
 from constrained_loaders import QueryFilter
 from constrained_loaders.sa.context import SALoaderBuilderContext
 
 
 class SAQueryFilter(QueryFilter[SALoaderBuilderContext]):
+    def __init__(
+        self,
+        clause_factory: Callable[[Any], Any],
+        sub_query_name: Optional[str] = None,
+        required_extensions: Optional[List[str]] = None,
+    ):
+        super().__init__(required_extensions)
+        self._sub_query_name = sub_query_name
+        self._clause_factory = clause_factory
+
     def apply_filter(
         self, context: SALoaderBuilderContext, reference_value: Optional[Any]
     ) -> SALoaderBuilderContext:
-        pass
+        context.add_filter(
+            functools.partial(self._modify_query, reference_value=reference_value)
+        )
+        return context
+
+    def _modify_query(
+        self, context: SALoaderBuilderContext, reference_value: Optional[Any]
+    ):
+        clause = self._clause_factory(reference_value)

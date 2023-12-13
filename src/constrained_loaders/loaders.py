9f3f6018ca3d54741f2b9de58ce2781ca5666a7b
Petr Jindra <petr@gbol.cz>
2023-12-13 21:33:11 +0100

    implementation of the FactoryLoader (generic loader mainly used for tests)

--- src/constrained_loaders/loaders.py
+++ src/constrained_loaders/loaders.py
@@ -4,7 +4,7 @@
 
 from __future__ import annotations
 
-from typing import Iterable
+from typing import Iterable, Callable, Sequence, Self
 
 from constrained_loaders.abstraction import Loader, T
 
@@ -17,3 +17,36 @@ class DummyLoader(Loader[T]):
 
     def __len__(self) -> int:
         return 0
+
+
+class FactoryLoader(Loader[T]):
+    """Return items provided by the `item_sequence_factory` callback.
+
+    This loader can be used for testing or as wrapper.
+    """
+
+    def __init__(self, item_sequence_factory: Callable[[], Sequence[T]]):
+        self._item_sequence_factory = item_sequence_factory
+        self._prepared_sequence = None
+
+    def __iter__(self) -> Iterable[T]:
+        s = self._prepare_sequence()
+        self._prepared_sequence = None
+        return s
+
+    def __len__(self) -> int:
+        return len(self._prepared_sequence())
+
+    @classmethod
+    def from_iterable(cls, item_sequence: Iterable[T]) -> Self:
+        """Create new instance from an iterable of items. The iterable is consumed when creating items sequence used in
+        the loader instance"""
+        items = list(item_sequence)
+        return cls(lambda: items)
+
+    def _prepare_sequence(self) -> Sequence[T]:
+        """Prepare the sequence of items and store it into instance state. If sequence is prepared, only returns the
+        sequence."""
+        if self._prepared_sequence is None:
+            self._prepared_sequence = self._item_sequence_factory()
+        return self._prepared_sequence
